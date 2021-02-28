ad84840437c9930f1a8fb0d5b1677415e1fe8e0a
Rushad Mehta <rushadmehta16@gmail.com>
2021-02-28 23:08:24 +0530

    Removed redundant code

--- connection_manager.py
+++ connection_manager.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import WebSocket, WebSocketDisconnect
+from fastapi import WebSocket
 
 
 class ConnectionManager:
--- data_models.py
+++ data_models.py
@@ -4,8 +4,3 @@ from pydantic import BaseModel
 class UserCredentials(BaseModel):
     username: str
     password: str
-
-class Message(BaseModel):
-    sent_by: str
-    date_time: str
-    message: str
