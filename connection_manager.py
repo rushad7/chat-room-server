b4ff24d1b28d68591defcc1f4836e514f252c142
Rushad Mehta <rushadmehta16@gmail.com>
2021-03-08 20:57:23 +0530

    Added type hinting

--- connection_manager.py
+++ connection_manager.py
@@ -4,19 +4,19 @@ from fastapi import WebSocket
 
 class ConnectionManager:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.active_connections: Dict[str, WebSocket] = {}
 
-    async def connect(self, uid, websocket: WebSocket):
+    async def connect(self, uid: str, websocket: WebSocket) -> None:
         await websocket.accept()
         self.active_connections[uid] = websocket
 
-    def disconnect(self, uid):
+    def disconnect(self, uid: str) -> None:
         self.active_connections.pop(uid)
 
-    async def send_message(self, message: str, websocket: WebSocket):
+    async def send_message(self, message: str, websocket: WebSocket) -> None:
         await websocket.send_text(message)
 
-    async def broadcast_message(self, message: str):
+    async def broadcast_message(self, message: str) -> None:
         for connection in list(self.active_connections.values()):
             await connection.send_text(message)
