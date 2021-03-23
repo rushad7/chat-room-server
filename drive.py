dd99ee21774cd2910fb550fb6bc211de96cb19c8
Rushad Mehta <rushadmehta16@gmail.com>
2021-03-23 22:37:53 +0530

    Fixed crash on auth

--- drive.py
+++ drive.py
@@ -1,36 +1,23 @@
 import os
+import json
 from pydrive.drive import GoogleDrive
-from pydrive.auth import GoogleAuth
+from pydrive.auth import GoogleAuth, ServiceAccountCredentials
 
 
 class ChatDrive:
     
     def __init__(self):
 
-        SECRETS = os.environ['secrets']
-        CREDS1 = os.environ['creds1']
-        CREDS2 = os.environ['creds2']
-        CREDS = CREDS1 + CREDS2
-
-        with open("client_secrets.json", "w") as secrets_file:
-            secrets_file.write(SECRETS)
-
-        with open("creds.txt", "w") as creds_file:
-            creds_file.write(CREDS)
-
         gauth = GoogleAuth()
-        gauth.LoadCredentialsFile("creds.txt")
-        self.is_expired = gauth.access_token_expired
+        SECRETS = json.loads(os.environ.get('secrets'))
+        scope = ['https://www.googleapis.com/auth/drive']
+        
+        with open("client_secrets.json", "w") as secrets_file:
+            json.dump(SECRETS, secrets_file)
 
-        if gauth.credentials is None:
-            gauth.LocalWebserverAuth()
-        elif self.is_expired:
-            gauth.Refresh()
-        else:
-            gauth.Authorize()
-
-        gauth.SaveCredentialsFile("creds.txt")
+        gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secrets.json', scope)
         self.drive = GoogleDrive(gauth)
+        self.is_expired = gauth.access_token_expired
 
 
     def create_room(self, name: str):
