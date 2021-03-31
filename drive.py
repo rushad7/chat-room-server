6a65584066a22535bc48d344ca4dedfcde0b84bb
Rushad Mehta <rushadmehta16@gmail.com>
2021-03-31 19:19:54 +0530

    Modified room checker logic

--- drive.py
+++ drive.py
@@ -13,7 +13,7 @@ class ChatDrive:
 
         DATABASE_URL = os.environ.get('DATABASE_URL')
         db = DataBase(DATABASE_URL)
-        if ~self.room_exists("global"):
+        if not self.room_exists("global"):
             query = Query.create_room("global")
             db.execute_query(query, logging_message="Create Global Room")
 
@@ -22,7 +22,7 @@ class ChatDrive:
         roomname  = f"{name}.room"
         room_exists = self.room_exists(roomname)
 
-        if ~room_exists:
+        if not room_exists:
             self.drive.CreateFile({'title': roomname})
         else:
             room = self.drive.CreateFile({'title': roomname})
@@ -49,10 +49,13 @@ class ChatDrive:
             return file_list[0]
 
     
-    def room_exists(self, roomname: str) -> bool:
-        files = self.drive.ListFile({'q': f"title='{roomname}' and trashed=false"}).GetList()
-        file_list = [file['title'] for file in files]
-        
-        if roomname in file_list:
+    def room_exists(self, roomname: str) -> bool or FileExistsError or FileNotFoundError:
+        files = self.drive.ListFile({'q': f"title='{roomname}.room' and trashed=false"}).GetList()
+        file_list = [file['id'] for file in files]
+ 
+        if len(file_list) > 1:
+            raise FileExistsError
+        elif len(file_list) == 0:
+            raise FileNotFoundError
+        else:
             return True
-        return False 
