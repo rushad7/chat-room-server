bf81d3b040d51672b7beebb45411ec4783fa134b
Rushad Mehta <rushadmehta16@gmail.com>
2021-04-03 14:21:33 +0530

    Fixed hash error on active users request from connection manager

--- connection_manager.py
+++ connection_manager.py
@@ -1,15 +1,15 @@
-from typing import Dict
+from typing import Dict, Tuple
 from fastapi import WebSocket
 
 
 class ConnectionManager:
 
     def __init__(self) -> None:
-        self.active_connections: Dict[str, WebSocket] = {}
+        self.active_connections: Dict[str, Tuple[WebSocket]] = {}
 
     async def connect(self, uid: str, websocket: WebSocket) -> None:
         await websocket.accept()
-        self.active_connections[uid] = websocket
+        self.active_connections[uid] = tuple(websocket)
 
     def disconnect(self, uid: str) -> None:
         self.active_connections.pop(uid)
@@ -17,9 +17,10 @@ class ConnectionManager:
     async def send_message(self, message: str, websocket: WebSocket) -> None:
         await websocket.send_text(message)
 
-    async def broadcast_message(self, websocket: WebSocket, message: str) -> None:
+    async def broadcast_message(self, sender_websocket: WebSocket, message: str) -> None:
         for connection in list(self.active_connections.values()):
-            if websocket == connection:
+            connection = connection[0]
+            if sender_websocket == connection:
                 continue
             else:
                 await connection.send_text(message)
