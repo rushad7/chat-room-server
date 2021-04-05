ccdaf402cef4b4d692516ad684d00f2f9fddef5e
Rushad Mehta <rushadmehta16@gmail.com>
2021-04-05 20:05:41 +0530

    Created logger

---
+++ logger.py
@@ -0,0 +1,47 @@
+import logging
+
+
+class Logger:
+
+    def __init__(self, logger_name: str, file: str, level=logging.DEBUG) -> None:
+
+        self._log = logging.getLogger(logger_name)
+        self._log.setLevel(level)
+        
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        fh = logging.FileHandler(file)
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+        self._log.addHandler(fh)
+
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(formatter)
+        self._log.addHandler(ch)
+
+
+    def debug(self, message: str):
+        self._log.debug(message)
+        self._exit()
+
+
+    def info(self, message: str):
+        self._log.info(message)
+        self._exit()
+
+
+    def warning(self, message: str):
+        self._log.warning(message)
+        self._exit()
+
+    
+    def error(self, message: str):
+        self._log.error(message)
+        self._exit()
+
+
+    def _exit(self):
+        for handler in self._log.handlers:
+            handler.close()
+            self._log.removeFilter(handler)
