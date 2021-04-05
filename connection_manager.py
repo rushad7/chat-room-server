dd63af5c63cc9a3e30834d7f0f590f3b625febff
Rushad Mehta <rushadmehta16@gmail.com>
2021-04-05 20:06:26 +0530

    Removed redundant print statements and replaced with a logger.

--- connection_manager.py
+++ connection_manager.py
@@ -1,4 +1,5 @@
 from fastapi import WebSocket
+from logger import Logger
 
 
 class PairedList:
@@ -8,9 +9,9 @@ class PairedList:
         self.keys = [i[0] for i in self.pairs]
         self.values = [i[1] for i in self.pairs]
 
+
     def append(self, element: tuple) -> None:
-        print(f"PAIRS : {self.pairs}")
-        print(f"ELEMENT TO APPEND : {element}")
+
         if element[0] not in self.keys:
             self.pairs.append(element)
             self.keys = [i[0] for i in self.pairs]
@@ -20,21 +21,28 @@ class PairedList:
             self.values[key_index] = element[1]
             self.pairs = list(zip(self.keys, self.values))
 
+
     def remove(self, key):
-        print(f"PAIRS : {self.pairs}")
-        print(f"KEY ENTERED : {key}")
         key_index = self.keys.index(key)
         self.keys.pop(key_index)
         self.values.pop(key_index)
         self.pairs = list(zip(self.keys, self.values))
+
 
     def clear(self):
         self.pairs.clear()
         self.keys.clear()
         self.values.clear()
 
+
+    def get(self, key):
+        key_index = self.keys.index(key)
+        return self.values[key_index]
+
+
     def __str__(self) -> str:
         return str(self.pairs)
+
 
     def __repr__(self) -> str:
         return str(self.pairs)
@@ -44,17 +52,30 @@ class ConnectionManager:
 
     def __init__(self) -> None:
         self.active_connections = PairedList()
+        self.logger = Logger("CHATROOM-SERVER-LOG", "chatroom_server.log")
+        self.logger.info("Connection Manager Initialised")
+
 
     async def connect(self, uid: str, websocket: WebSocket) -> None:
-        await websocket.accept()
-        self.active_connections.append((uid, websocket))
-        print(f"INFO : {uid} connected via {websocket}")
+        try:
+            await websocket.accept()
+            self.active_connections.append((uid, websocket))
+            self.logger.info(f"Connected : {uid}")
+        except:
+            self.logger.error("Connection Failed")
+
 
     def disconnect(self, uid: str) -> None:
-        self.active_connections.remove(uid)
+        try:
+            self.active_connections.remove(uid)
+            self.logger.info(f"Disconnected : {uid}")
+        except:
+            pass
+
 
     async def send_message(self, message: str, websocket: WebSocket) -> None:
         await websocket.send_text(message)
+
 
     async def broadcast_message(self, sender_websocket: WebSocket, message: str) -> None:
         for connection in self.active_connections.values:
