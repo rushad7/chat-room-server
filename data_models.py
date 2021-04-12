43f4f4efa711047d08b2cb30277f29a7120566e9
Rushad Mehta <rushadmehta16@gmail.com>
2021-04-13 01:41:13 +0530

    Modified data models

--- data_models.py
+++ data_models.py
@@ -1,7 +1,6 @@
 # pylint: disable=no-name-in-module
 # pylint: disable=no-self-argument
 
-from typing import Union
 from pydantic import BaseModel
 
 
@@ -19,15 +18,5 @@ class Room(BaseModel):
     """ Class for data modelling of rooms
     """
     
-    name: str
-    creator: str
-
-
-class JoinRoom(BaseModel):
-
-    """ Class for data modelling of room joining
-    """
-
     roomname: str
     username: str
-    description: Union[str, None]
